434c9113ca858e50b1af5b655af7b6451ecaefa8
uw <ute.wagner@beuth-hochschule.de>
2024-09-15 17:39:53 +0200

    o Adapt to new GeomKernel interface with consistent naming. o Delete GeomKernel instance after use.

--- Assets/Scripts/STEPImporter.cs
+++ Assets/Scripts/STEPImporter.cs
@@ -11,14 +11,14 @@ namespace VENTUS.ModelImporter
       public static ModelObjectData ParseFile(string path)
       {
          int id = -1;
-         IntPtr geomKernel = ImportGeomKernel.initGeomKernelLOD(2);
+         IntPtr geomKernel = ImportGeomKernel.initGeomKernel(); // ToDo: geomKernel initialize/delete once per session
          if (geomKernel == IntPtr.Zero)
          {
             Debug.Log("geomKernel could not be initialized!");
             return null;
          }
-         ResultFileLoading loadResult = ImportGeomKernel.loadModelObjectFromFile(path, geomKernel, ref id);
-         IntPtr cppModelObject = ImportGeomKernel.getModelObject(geomKernel, id);
+         ResultFileLoading loadResult = ImportGeomKernel.loadObjectsFromFile(path, geomKernel, ref id);
+         IntPtr cppModelObject = ImportGeomKernel.getObject(geomKernel, id);
          if (loadResult != ResultFileLoading.CREATE_SUCCESSFUL || cppModelObject == IntPtr.Zero)
          {
             Debug.Log("STP file could not be loaded!");
@@ -35,19 +35,20 @@ namespace VENTUS.ModelImporter
             Texture = null
          };
 
-         int numberOfRoots = ImportGeomKernel.transferNumberOfRoots(cppModelObject);
+         int numberOfRoots = ImportGeomKernel.getNumberOfRoots(cppModelObject);
          for (int i = 0; i < numberOfRoots; ++i)
          {
-            IntPtr cppModelBase = ImportGeomKernel.requestRootFromObject(cppModelObject, i);
+            IntPtr cppModelBase = ImportGeomKernel.getRoot(cppModelObject, i);
             if (cppModelBase != IntPtr.Zero)
                modelParent.Children.Add(CreateModelObjectData(cppModelBase));
          }
+         ImportGeomKernel.cleanUpGeomKernel(geomKernel);
          return modelParent;
       }
 
       private static ModelObjectData CreateModelObjectData(IntPtr cppModelBase)
       {
-         int type = ImportGeomKernel.transferType(cppModelBase);
+         int type = ImportGeomKernel.getType(cppModelBase);
 
          switch (type)
          {
@@ -63,10 +64,10 @@ namespace VENTUS.ModelImporter
                      Texture = null
                   };
 
-                  int numberOfCildren = ImportGeomKernel.transferChildsNumberOfARoot(cppModelBase);
+                  int numberOfCildren = ImportGeomKernel.getNumberOfChildren(cppModelBase);
                   for (int j = 0; j < numberOfCildren; ++j)
                   {
-                     IntPtr cppChildModelBase = ImportGeomKernel.requestChildFromRoot(cppModelBase, j);
+                     IntPtr cppChildModelBase = ImportGeomKernel.getChild(cppModelBase, j);
                      if (cppChildModelBase != IntPtr.Zero)
                         modelObject.Children.Add(CreateModelObjectData(cppChildModelBase));
                   }
@@ -91,37 +92,37 @@ namespace VENTUS.ModelImporter
       private static string transferName(IntPtr cppModelBase)
       {
          int size = 0;
-         IntPtr name = ImportGeomKernel.transferName(cppModelBase, ref size);
+         IntPtr name = ImportGeomKernel.getName(cppModelBase, ref size);
          return Marshal.PtrToStringAnsi(name, size);
       }
 
       private static Matrix4x4 transferTransformation(IntPtr cppModelBase)
       {
-         Transformation transformation = new Transformation();
-         ImportGeomKernel.transferTransformation(cppModelBase, ref transformation);
-         Vector4 col1 = new Vector4(transformation.mA11, transformation.mA21, transformation.mA31, transformation.mA41);
-         Vector4 col2 = new Vector4(transformation.mA12, transformation.mA22, transformation.mA32, transformation.mA42);
-         Vector4 col3 = new Vector4(transformation.mA13, transformation.mA23, transformation.mA33, transformation.mA43);
-         Vector4 col4 = new Vector4(transformation.mA14, transformation.mA24, transformation.mA34, transformation.mA44);
-         Matrix4x4 mat = new Matrix4x4(col1, col2, col3, col4);
+         Transformation transformation = new();
+         ImportGeomKernel.getTransformation(cppModelBase, ref transformation);
+         Vector4 col1 = new(transformation.mA11, transformation.mA21, transformation.mA31, transformation.mA41);
+         Vector4 col2 = new(transformation.mA12, transformation.mA22, transformation.mA32, transformation.mA42);
+         Vector4 col3 = new(transformation.mA13, transformation.mA23, transformation.mA33, transformation.mA43);
+         Vector4 col4 = new(transformation.mA14, transformation.mA24, transformation.mA34, transformation.mA44);
+         Matrix4x4 mat = new(col1, col2, col3, col4);
          return OCCToUnity.adapt(mat);
       }
 
       private static Bounds transferBoundingBox(IntPtr cppModelBase)
       {
-         BoundingBox box = new BoundingBox();
-         ImportGeomKernel.transferBoundingBox(cppModelBase, ref box);
-         Coordinate3d ptMin = new Coordinate3d(box.mXmin, box.mYmin, box.mZmin);
-         Coordinate3d ptMax = new Coordinate3d(box.mXmax, box.mYmax, box.mZmax);
+         BoundingBox box = new();
+         ImportGeomKernel.getBoundingBox(cppModelBase, ref box);
+         Coordinate3d ptMin = new(box.mXmin, box.mYmin, box.mZmin);
+         Coordinate3d ptMax = new(box.mXmax, box.mYmax, box.mZmax);
          ptMin = OCCToUnity.adapt(ptMin);
          ptMax = OCCToUnity.adapt(ptMax);
 
-         Vector3 center = new Vector3();
+         Vector3 center = new();
          center.x = (float)(ptMin.X + ptMax.X) / 2;
          center.y = (float)(ptMin.Y + ptMax.Y) / 2;
          center.z = (float)(ptMin.Z + ptMax.Z) / 2;
 
-         Vector3 size = new Vector3();
+         Vector3 size = new();
          size.x = Mathf.Abs((float)(ptMax.X - ptMin.X));
          size.y = Mathf.Abs((float)(ptMax.Y - ptMin.Y));
          size.z = Mathf.Abs((float)(ptMax.Z - ptMin.Z));
@@ -136,25 +137,25 @@ namespace VENTUS.ModelImporter
          List<int> triangles = new();
 
          int coordCount = 0;
-         int nMeshes = ImportGeomKernel.transferNumberOfMeshes(cppModelBase);
+         int nMeshes = ImportGeomKernel.getNumberOfMeshes(cppModelBase);
          for (int i = 0; i < nMeshes; i++)
          {
-            IntPtr cppMesh = ImportGeomKernel.requestModelMesh(cppModelBase, i);
-            int nTrinagles = ImportGeomKernel.transferNumberOfTriangles(cppMesh);
+            IntPtr cppMesh = ImportGeomKernel.getMesh(cppModelBase, i);
+            int nTrinagles = ImportGeomKernel.getNumberOfTriangles(cppMesh);
             for (int j = 1; j <= nTrinagles; j++)
             {
                int ptIdx1 = 0, ptIdx2 = 0, ptIdx3 = 0;
-               ImportGeomKernel.transferTriangle(cppMesh, j, ref ptIdx1, ref ptIdx2, ref ptIdx3);
+               ImportGeomKernel.getTriangle(cppMesh, j, ref ptIdx1, ref ptIdx2, ref ptIdx3);
                triangles.Add(OCCToUnity.adapt(ptIdx1) + coordCount);
                triangles.Add(OCCToUnity.adapt(ptIdx2) + coordCount);
                triangles.Add(OCCToUnity.adapt(ptIdx3) + coordCount);
             }
-            int nCoord = ImportGeomKernel.transferCoordinateCount(cppMesh);
+            int nCoord = ImportGeomKernel.getNumberOfPoints(cppMesh);
             List<Coordinate3d> coordinates = new List<Coordinate3d>();
             for (int j = 1; j <= nCoord; j++)
             {
                Coordinate3d vertex = new();
-               ImportGeomKernel.transferCoordinate(cppMesh, j, ref vertex);
+               ImportGeomKernel.getPoint(cppMesh, j, ref vertex);
                vertex = OCCToUnity.adapt(vertex);
                vertices.Add(new Vector3((float)vertex.X, (float)vertex.Y, (float)vertex.Z));
 
@@ -172,14 +173,14 @@ namespace VENTUS.ModelImporter
 
       private static Color transferColor(IntPtr cppModelBase)
       {
-         int numberOfMeshes = ImportGeomKernel.transferNumberOfMeshes(cppModelBase);
+         int numberOfMeshes = ImportGeomKernel.getNumberOfMeshes(cppModelBase);
          if (numberOfMeshes > 0)
          {
             // multiple colors per part not supported yet - take color of first mesh
-            IntPtr cppMesh = ImportGeomKernel.requestModelMesh(cppModelBase, 0);
-            IntPtr cppGraphicInfo = ImportGeomKernel.requestGraphicInfo(cppMesh);
-            RGBAColor color = new RGBAColor();
-            ImportGeomKernel.transferColor(cppGraphicInfo, ref color);
+            IntPtr cppMesh = ImportGeomKernel.getMesh(cppModelBase, 0);
+            IntPtr cppGraphicInfo = ImportGeomKernel.getGraphicInfo(cppMesh);
+            RGBAColor color = new();
+            ImportGeomKernel.getColor(cppGraphicInfo, ref color);
             return new Color((float)color.mR, (float)color.mB, (float)color.mG, (float)color.mA);
          }
          return new Color();
@@ -187,7 +188,7 @@ namespace VENTUS.ModelImporter
 
       private static Texture2D transferTexture(IntPtr cppModelBase)
       {
-         // TODO: ImportGeomKernel.transferTexture(..);
+         // TODO: ImportGeomKernel.getTexture(..);
          return new Texture2D(1, 1);
       }
    }
--- Assets/UnitySTEPImporter/Runtime/Scripts/GeomKernel/ImportGeomKernel.cs
+++ Assets/UnitySTEPImporter/Runtime/Scripts/GeomKernel/ImportGeomKernel.cs
@@ -4,49 +4,57 @@ using System.Runtime.InteropServices;
 namespace VENTUS.UnitySTEPImporter.GeomKernel
 {
 
-    // ExportGeomKernel DLL API
-    public class ImportGeomKernel {
-        // C:\\Users\\admin2\\Desktop\\VENTUS2\\UnitySTEPImporter_dev_II\\Assets\\UnitySTEPImporter\\Runtime\\DLLs\\
-        public const string DLLName = "VENTUS_GeomKernel.dll";
+   // ExportGeomKernel DLL API
+   public class ImportGeomKernel
+   {
+      // C:\\Users\\admin2\\Desktop\\VENTUS2\\UnitySTEPImporter_dev_II\\Assets\\UnitySTEPImporter\\Runtime\\DLLs\\
+      public const string DLLName = "VENTUS_GeomKernel.dll";
 
-        /* === Export_GeomKernel === */
-        [DllImport(DLLName, EntryPoint = "initGeomKernelLOD", CallingConvention = CallingConvention.StdCall)] public unsafe static extern IntPtr initGeomKernelLOD(int defaultLOD);
-        [DllImport(DLLName, EntryPoint = "loadModelObjectFromFile", CallingConvention = CallingConvention.StdCall)] public unsafe static extern ResultFileLoading loadModelObjectFromFile(string filePath, IntPtr geomKernel, ref int objectManagerID);
-        [DllImport(DLLName, EntryPoint = "getModelObject", CallingConvention = CallingConvention.StdCall)] public unsafe static extern IntPtr getModelObject(IntPtr cppKernel, int objectManagerID);
+      [DllImport(DLLName, EntryPoint = "initGeomKernel", CallingConvention = CallingConvention.StdCall)]
+      public unsafe static extern IntPtr initGeomKernel();
+      [DllImport(DLLName, EntryPoint = "cleanUpGeomKernel", CallingConvention = CallingConvention.StdCall)]
+      public unsafe static extern void cleanUpGeomKernel(IntPtr cppKernel);
 
+      /* === Export_Assembly === */
+      [DllImport(DLLName, EntryPoint = "loadObjectsFromFile", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern ResultFileLoading loadObjectsFromFile(string filePath, IntPtr geomKernel, ref int objID);
+      [DllImport(DLLName, EntryPoint = "getObject", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern IntPtr getObject(IntPtr cppKernel, int objID);
+      [DllImport(DLLName, EntryPoint = "getNumberOfRoots", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern int getNumberOfRoots(IntPtr modelObject);
+      [DllImport(DLLName, EntryPoint = "getRoot", CallingConvention = CallingConvention.StdCall)]
+      public unsafe static extern IntPtr getRoot(IntPtr modelObject, int index);
+      [DllImport(DLLName, EntryPoint = "getNumberOfChildren", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern int getNumberOfChildren(IntPtr modelProduct);
+      [DllImport(DLLName, EntryPoint = "getChild", CallingConvention = CallingConvention.StdCall)]
+      public unsafe static extern IntPtr getChild(IntPtr modelProduct, int index);
 
-        /* === Export_Assembly === */
-        [DllImport(DLLName, EntryPoint = "transferNumberOfRoots", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferNumberOfRoots(IntPtr modelObject);
-        [DllImport(DLLName, EntryPoint = "transferChildsNumberOfARoot", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferChildsNumberOfARoot(IntPtr modelProduct);
-        [DllImport(DLLName, EntryPoint = "transferNumberOfParts", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferNumberOfParts(IntPtr modelObject);
-        [DllImport(DLLName, EntryPoint = "transferNumberOfProducts", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferNumberOfProducts(IntPtr modelObject);
-        [DllImport(DLLName, EntryPoint = "transferNumberOfChildParts", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferNumberOfChildParts(IntPtr modelProduct);
-        [DllImport(DLLName, EntryPoint = "transferNumberOfChildProducts", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferNumberOfChildProducts(IntPtr modelProduct);
+      /* === Export_MeshAttribute === */
+      [DllImport(DLLName, EntryPoint = "getNumberOfMeshes", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern int getNumberOfMeshes(IntPtr modelPart);
+      [DllImport(DLLName, EntryPoint = "getMesh", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern IntPtr getMesh(IntPtr modelPart, int index);
+      [DllImport(DLLName, EntryPoint = "getNumberOfTriangles", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern int getNumberOfTriangles(IntPtr modelMesh);
+      [DllImport(DLLName, EntryPoint = "getTriangle", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern void getTriangle(IntPtr modelMesh, int index, ref int point1, ref int point2, ref int point3);
+      [DllImport(DLLName, EntryPoint = "getNumberOfPoints", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern int getNumberOfPoints(IntPtr modelMesh);
+      [DllImport(DLLName, EntryPoint = "getPoint", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern void getPoint(IntPtr modelMesh, int index, ref Coordinate3d coordinateArray);
+      [DllImport(DLLName, EntryPoint = "getGraphicInfo", CallingConvention = CallingConvention.StdCall)]
+      public unsafe static extern IntPtr getGraphicInfo(IntPtr modelMesh);
+      [DllImport(DLLName, EntryPoint = "getColor", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern void getColor(IntPtr graphicinfo, ref RGBAColor RGBAColor);
 
-        /* === Export_MeshAttribute === */
-        [DllImport(DLLName, EntryPoint = "transferNumberOfMeshes", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferNumberOfMeshes(IntPtr modelPart);
-        [DllImport(DLLName, EntryPoint = "requestModelMesh", CallingConvention = CallingConvention.StdCall)] public unsafe static extern IntPtr requestModelMesh(IntPtr modelPart, int indicesArray);
-        [DllImport(DLLName, EntryPoint = "transferNumberOfTriangles", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferNumberOfTriangles(IntPtr modelMesh);
-        [DllImport(DLLName, EntryPoint = "transferTriangle", CallingConvention = CallingConvention.StdCall)] public unsafe static extern void transferTriangle(IntPtr modelMesh, int index, ref int point1, ref int point2, ref int point3);
-        [DllImport(DLLName, EntryPoint = "transferCoordinateCount", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferCoordinateCount(IntPtr modelMesh);
-        [DllImport(DLLName, EntryPoint = "transferCoordinate", CallingConvention = CallingConvention.StdCall)] public unsafe static extern void transferCoordinate(IntPtr modelMesh, int index, ref Coordinate3d coordinateArray);
-        [DllImport(DLLName, EntryPoint = "transferLodStatus", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferLodStatus(IntPtr modelMesh);
-        [DllImport(DLLName, EntryPoint = "transferColor", CallingConvention = CallingConvention.StdCall)] public unsafe static extern void transferColor(IntPtr graphicinfo, ref RGBAColor RGBAColor);
-
-        /* === Export_ModelBaseAttribute === */
-        [DllImport(DLLName, EntryPoint = "transferType", CallingConvention = CallingConvention.StdCall)] public unsafe static extern int transferType(IntPtr modelBase);
-        [DllImport(DLLName, EntryPoint = "transferTransformation", CallingConvention = CallingConvention.StdCall)] public unsafe static extern void transferTransformation(IntPtr modelBase, ref Transformation Transformation);
-        [DllImport(DLLName, EntryPoint = "transferID", CallingConvention = CallingConvention.StdCall)] public unsafe static extern void transferID(IntPtr modelBase, ref int refID);
-        [DllImport(DLLName, CharSet = CharSet.Ansi, EntryPoint = "transferName", CallingConvention = CallingConvention.StdCall)] public unsafe static extern IntPtr transferName(IntPtr modelBase, ref int size);
-        [DllImport(DLLName, EntryPoint = "transferBoundingBox", CallingConvention = CallingConvention.StdCall)] public unsafe static extern void transferBoundingBox(IntPtr modelBase, ref BoundingBox BoundingBox);
-    
-        /* === Export_ModelLODChanger === */
-        [DllImport(DLLName)] public unsafe static extern bool changeLODModelpart(IntPtr geomKernel, IntPtr modelpart, int lod);
-
-        /* === Export_Pointer === */
-        [DllImport(DLLName, EntryPoint = "requestModelobject", CallingConvention = CallingConvention.StdCall)] public unsafe static extern IntPtr requestModelobject(IntPtr geomKernel, int objID);
-        [DllImport(DLLName, EntryPoint = "requestRootFromObject", CallingConvention = CallingConvention.StdCall)] public unsafe static extern IntPtr requestRootFromObject(IntPtr modelObject, int index);
-        [DllImport(DLLName, EntryPoint = "requestChildFromRoot", CallingConvention = CallingConvention.StdCall)] public unsafe static extern IntPtr requestChildFromRoot(IntPtr modelProduct, int index);
-        [DllImport(DLLName, EntryPoint = "requestGraphicInfo", CallingConvention = CallingConvention.StdCall)] public unsafe static extern IntPtr requestGraphicInfo(IntPtr modelMesh);        
-    }
+      /* === Export_ModelBaseAttribute === */
+      [DllImport(DLLName, EntryPoint = "getType", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern int getType(IntPtr modelBase);
+      [DllImport(DLLName, EntryPoint = "getTransformation", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern void getTransformation(IntPtr modelBase, ref Transformation Transformation);
+      [DllImport(DLLName, CharSet = CharSet.Ansi, EntryPoint = "getName", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern IntPtr getName(IntPtr modelBase, ref int size);
+      [DllImport(DLLName, EntryPoint = "getBoundingBox", CallingConvention = CallingConvention.StdCall)] 
+      public unsafe static extern void getBoundingBox(IntPtr modelBase, ref BoundingBox BoundingBox);
+   }
 }
